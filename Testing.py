505a1852ba1a2c0b311c5b029eaebb992973337e
Yonsei-MILab <52816867+Yonsei-MILab@users.noreply.github.com>
2022-12-21 16:06:44 +0900

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -18,37 +18,30 @@ import pdb
 # specify which GPU(s) to be used
 device = torch.device('cuda:0')
 
-# Load in-vivo test data
+# Load training dataset
 #dataset = io.loadmat('Data/training_dataset.mat')
-feasibility = torch.tensor(dataset['dataset_training'])
-feasibility_1 = torch.tensor(feasibility.clone())
-feasibility_extend = np.expand_dims(feasibility_1,1)
-feasibility_extend_1 = torch.tensor(feasibility_extend)
+training_syn_dataset = torch.tensor(dataset['dataset_training'])
+training_syn_dataset = np.expand_dims(training_syn_dataset,1)
+training_syn_dataset = torch.tensor(training_syn_dataset)
 
 training_mask = torch.tensor(dataset['dataset_mask'])
-training_mask_1 = torch.tensor(training_mask.clone())
-training_mask_extend = np.expand_dims(training_mask_1,1)
-training_mask_extend_1 = torch.tensor(training_mask_extend)
+training_mask = np.expand_dims(training_mask,1)
+training_mask = torch.tensor(training_mask)
 
 
 training_label = torch.tensor(dataset['dataset_label'])
-training_label_1 = torch.tensor(training_label.clone())
-training_label_extend = np.expand_dims(training_label_1,1)
-training_label_extend_1 = torch.tensor(training_label_extend)
+training_label = np.expand_dims(training_label,1)
+training_label = torch.tensor(training_label)
 
 
 kernel_h, kernel_w = 11, 11
 step, n_channels = 1, 1
-feasibility_patch_1 = feasibility_extend_1.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w)
+training_syn_dataset_patch = np.array(training_syn_dataset.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w))
 
-mask_patch_1 = training_mask_extend_1.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w)
+training_mask_patch = np.array(training_mask.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w))
 
-label_patch_1 = training_label_extend_1.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w)
-label_patch_1 = label_patch_1[:,:,5,5]
-
-label_patch = np.array(label_patch_1)
-mask_patch = np.array(mask_patch_1)
-feasibility_patch = np.array(feasibility_patch_1)
+training_label_patch = training_label.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w)
+training_label_patch = np.array(training_label_patch[:,:,5,5])
 
 
 from numba import jit
@@ -80,9 +73,9 @@ def combine_final(x,y):
 class Custom_dataset(Dataset):
     #Dataset = int(str(Dataset), 16)
     #@jit(nopython=True)         
-    def __init__(self,feasibility_patch):
-        self.feasibility_patch = (feasibility_patch)
-        self.mask_patch = (mask_patch)
+    def __init__(self,training_syn_dataset_patch):
+        self.training_syn_dataset_patch = (training_syn_dataset_patch)
+        self.training_mask_patch = (training_mask_patch)
 
     
       
@@ -90,16 +83,16 @@ class Custom_dataset(Dataset):
     def __len__(self):
         
    
-        return len(self.feasibility_patch)
+        return len(self.training_syn_dataset_patch)
     
                
     def __getitem__(self,idx):
-        x = combine_final(self.feasibility_patch[idx], mask_patch[idx])
-        y = label_patch[idx] 
+        x = combine_final(self.training_syn_dataset_patch[idx], training_mask_patch[idx])
+        y = training_label_patch[idx] 
         return x,y
 
 
-dataset = Custom_dataset(feasibility_patch)
+dataset = Custom_dataset(training_syn_dataset_patch)
 dataloader = DataLoader(dataset,batch_size= 100000,shuffle=True)
 
 # Network

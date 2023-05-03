edd91dda69c1c4ece5546a19005627b47f661a4e
Yonsei-MILab <52816867+Yonsei-MILab@users.noreply.github.com>
2023-05-03 22:21:31 +0900

    Update Training.py

--- Training.py
+++ Training.py
@@ -34,6 +34,7 @@ training_label = np.expand_dims(training_label,1)
 training_label = torch.tensor(training_label)
 
 
+# Patch the dataset 11 by 11
 kernel_h, kernel_w = 11, 11
 step, n_channels = 1, 1
 training_syn_dataset_patch = np.array(training_syn_dataset.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w))
@@ -41,27 +42,32 @@ training_syn_dataset_patch = np.array(training_syn_dataset.unfold(2, kernel_h, s
 training_mask_patch = np.array(training_mask.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w))
 
 training_label_patch = training_label.unfold(2, kernel_h, step).unfold(3, kernel_w, step).permute(2, 3, 0, 1, 4, 5).reshape(-1, n_channels, kernel_h, kernel_w)
+
+# Assign the label (Center pixels for 11 by 11 patched images)
 training_label_patch = np.array(training_label_patch[:,:,5,5])
 
 
 from numba import jit
-
-@jit(nopython=True)        
+@jit(nopython=True)
+# Adding noise function
 def add_noise(x):
     noise = np.random.normal(0,0.013,x.shape)
     return x+noise
 
-@jit(nopython=True)        
+@jit(nopython=True)
+# Adding magnitude bias function based on normalization constant: nor_factor
 def add_bias(x, nor_factor):
     bias = nor_factor*np.abs(x+(0.1*np.random.rand(1)))*np.exp(1j*np.angle(x))
     return bias
 
-@jit(nopython=True)        
+@jit(nopython=True)     
+#Adding noise function for complex valued data
 def add_noise_to_complex(x,y):
     z = (add_noise(np.real(x))+1j*add_noise(np.imag(x)))*y
     return z    
 
-@jit(nopython=True)        
+@jit(nopython=True)
+#Adding normalization & noise function function 
 def combine_final(x,y):
     nor_factor = (1.3-0.7)*np.random.rand(1)+ 0.7
     xx = add_noise_to_complex(add_bias(x,nor_factor), y)
@@ -96,15 +102,15 @@ class EPT_Model(nn.Module):
             nn.Linear(242, 1024),
             nn.BatchNorm1d(1024),
             nn.ReLU(),
-            nn.Dropout(0.2)
+            nn.Dropout(0.4)
             nn.Linear(1024, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Dropout(0.2)
+            nn.Dropout(0.4)
             nn.Linear(512, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Dropout(0.2)
+            nn.Dropout(0.4)
             nn.Linear(512, 1),
             nn.Sigmoid()
         )
@@ -117,7 +123,6 @@ print(model)
 
 # loss function
 criterion = nn.MSELoss()
-
 
 # optimizer
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
